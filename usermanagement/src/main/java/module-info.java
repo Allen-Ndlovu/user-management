b535cd5b33ccae5bf53eb4429d9fa1e462a3b0b4
Allen-Ndlovu <ndlovuallen58@gmail.com>
2025-04-06 18:51:06 +0200

    Delete usermanagement directory

--- usermanagement/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.visionaryann.production.usermanagement {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.visionaryann.production.usermanagement to javafx.fxml;
-    exports com.visionaryann.production.usermanagement;
-}
